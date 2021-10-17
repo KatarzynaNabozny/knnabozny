a60ce8f9cedfb52c2aa06dfc7ea58416b8dad0c1
Katarzyna Nabożny <knnabozny@gmail.com>
2021-10-17 18:18:31 +0200

    7.1 Added kodilla-stream module

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,23 @@
 package com.kodilla.stream;
 
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.book.BookDirectory;
 
 
 public class StreamMain {
 
     public static void main(String[] args) {
-       BookDirectory theBookDirectory = new BookDirectory();
-       theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() >2005)
-                .forEach(System.out::println);
-    }
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Some text 1", text -> text);
+        poemBeautifier.beautify("Some text 2", text -> text.toUpperCase());
+        poemBeautifier.beautify("Some text 3", text -> text.toLowerCase());
+        poemBeautifier.beautify("Some text 4", text -> text + "*");
 
+        // BookDirectory theBookDirectory = new BookDirectory();
+        // theBookDirectory.getList().stream()
+        //  .filter(book -> book.getYearOfPublication() >2005)
+        //  .forEach(System.out::println);
+
+    }
 }

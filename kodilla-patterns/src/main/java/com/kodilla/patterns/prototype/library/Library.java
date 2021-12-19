2a9f065cf6d136e59afa6162e0abdbd4ecdfba0f
Katarzyna Nabożny <knnabozny@gmail.com>
2021-12-19 19:27:59 +0100

    15.2 Added kodilla-patterns module

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,14 +1,29 @@
 package com.kodilla.patterns.prototype.library;
 
+import java.sql.ClientInfoStatus;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-public final class Library {
+public final class Library extends Prototype<Library> {
 
-    final String name;
-    final Set<Book> books = new HashSet<>();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Objects.equals(name, library.name) && Objects.equals(books, library.books);
+    }
 
-    public Library(final String name) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, books);
+    }
+
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
         this.name = name;
     }
 
@@ -18,6 +33,20 @@ public final class Library {
 
     public Set<Book> getBooks() {
         return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books) {
+            clonedLibrary.getBooks().add(book);
+        }
+        return clonedLibrary;
+
     }
 
 }
--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,12 +1,47 @@
 package com.kodilla.patterns.prototype.library;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LibraryTestSuite {
 
     @Test
-    void testGetBooks(){
+    void testGetBooks() {
+        //given
+        Library library = new Library("Library");
+
+        List<Book> listBook = new ArrayList<Book>();
+        Book book1 = new Book("Hr", "Br", LocalDate.now());
+        listBook.add(book1);
+        listBook.add(new Book("Cr", "Cr", LocalDate.now()));
+        listBook.add(new Book("Dr", "Dr", LocalDate.now()));
+        library.getBooks().addAll(listBook);
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.getName();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.getName();
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+        //when
+        library.getBooks().remove(book1);
+
+        //then
+
+        Assertions.assertEquals(2, clonedLibrary.getBooks().size());
+        Assertions.assertEquals(3, deepClonedLibrary.getBooks().size());
+        Assertions.assertEquals(2, library.getBooks().size());
 
     }
-
 }
